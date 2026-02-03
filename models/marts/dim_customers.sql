ca6ce3bea0857a216f70b9bc0495eb2b21638525
Wilfredo Contreras <wil.contreras@gmail.com>
2026-02-02 19:42:55 -0800

    added 2 staging models refactored dim_customers established materialization strategy

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,23 +1,12 @@
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
+    select * from {{ ref('stg_jaffle_shop_customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
+    select * from {{ ref('stg_jaffle_shop_orders') }}
 
 ),
 
@@ -25,17 +14,14 @@ customer_orders as (
 
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
-
     from orders
 
     group by 1
 
 ),
-
 
 final as (
 
@@ -46,10 +32,10 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
     from customers
-
-    left join customer_orders using (customer_id)
+    left join
+        customer_orders
+        on customers.customer_id = customer_orders.customer_id
 
 )
 
---
+++ models/staging/stg_jaffle_shop_orders.sql
@@ -0,0 +1,7 @@
+SELECT
+    id AS order_id,
+    user_id AS customer_id,
+    order_date,
+    status
+FROM
+    raw.jaffle_shop.orders
